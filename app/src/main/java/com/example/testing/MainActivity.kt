ef3c8d4762383cca8efe96c5570eca8e53ac7f85
AniruddhaKulk <anikulki@gmail.com>
2025-03-26 10:07:13 +0530

    fixed

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.testing.ui.theme.TestingTheme
+import com.example.testing.ui.theme.ToDoListScreenRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
